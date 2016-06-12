a0c8ebc56166d0cdb6df20b0dc46fd88e7b53d03
Your Name <tantohyung@gmail.com>
2016-06-12 06:45:32 +0000

    set up validations

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -10,14 +10,23 @@ class PlacesController < ApplicationController
   end
 
   def create
-    current_user.places.create(place_params)
-    redirect_to root_path    
+    @place = current_user.places.create(place_params)
+    if @place.valid? 
+      redirect_to root_path    
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit    
     @place = Place.find(params[:id])
     if @place.user != current_user
       return render text: 'Not Allowed', status: :forbidden
+    end
+    if @place.valid? 
+      redirect_to root_path    
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
