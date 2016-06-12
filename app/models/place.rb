e7a948a08ef45b75faaee9dfc075a7d97d63730e
Your Name <tantohyung@gmail.com>
2016-06-12 02:05:44 +0000

    Linking Place and User models

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,4 +2,6 @@ class Place < ActiveRecord::Base
   validates :name, presence: true, length: { minimum: 3 }
   validates :address, presence: true
   validates :description, presence: true
+
+  belongs_to :user
 end
