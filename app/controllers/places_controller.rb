704d076359a3e3ff959245a897e91d77d4ab9258
Your Name <tantohyung@gmail.com>
2016-06-12 06:16:44 +0000

    Implement delete and edit functionality

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :destroy]
 
   def index
     @places = Place.paginate(:page => params[:page], :per_page => 3)
@@ -16,6 +16,18 @@ class PlacesController < ApplicationController
 
   def edit
     @place = Place.find(params[:id])
+  end
+
+  def update
+    @place = Place.find(params[:id])
+    @place.update_attributes(place_params)
+    redirect_to root_path
+  end
+
+  def destroy
+    @place = Place.find(params[:id])
+    @place.destroy
+    redirect_to root_path
   end
 
   def show    
