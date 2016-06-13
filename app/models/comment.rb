3247828506be581ae413b91d4721e3d0694de45a
Your Name <tantohyung@gmail.com>
2016-06-13 12:38:19 +0000

    Complete comments section

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,16 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :place
+
+  RATINGS = {
+    '&#x2605'.html_safe                               => '1_star',
+    '&#x2605&#x2605'.html_safe                        => '2_stars',
+    '&#x2605&#x2605&#x2605'.html_safe                 => '3_stars',
+    '&#x2605&#x2605&#x2605&#x2605'.html_safe          => '4_stars',
+    '&#x2605&#x2605&#x2605&#x2605&#x2605'.html_safe   => '5_stars',
+  }
+
+  def humanized_rating
+    RATINGS.invert[self.rating]
+  end
 end
